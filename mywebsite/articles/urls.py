f6f51728754161dd4e8cd3b528b37cb6b342f83d
Carlos Miguel Valderrama <81756261+ascxiao@users.noreply.github.com>
2026-03-18 04:15:17 +0000

    FEAT: Added delete and edit functionalities

--- mywebsite/articles/urls.py
+++ mywebsite/articles/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='articles_index'),
     path('add/', views.add, name='articles_add'),
+    path('delete/', views.delete, name='articles_delete'),
+    path('<int:article_id>/', views.detail, name='articles_detail'),
+    path('<int:article_id>/delete/', views.delete, name='articles_delete'),
+    path('<int:article_id>/edit/', views.edit, name='articles_edit'),
 ]
--- mywebsite/articles/views.py
+++ mywebsite/articles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotAllowed
 from .models import Article
 
 
@@ -16,3 +17,29 @@ def add(request):
             Article.objects.create(title=title, content=content, author=author)
             return redirect('articles_index')
     return render(request, 'articles_add.html')
+
+def delete(request, article_id):
+    if request.method == 'POST':
+        article = get_object_or_404(Article, id=article_id)
+        article.delete()
+        return redirect('articles_index')
+    return HttpResponseNotAllowed(['POST'])
+
+def edit(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        title = request.POST.get('title', '').strip()
+        content = request.POST.get('content', '').strip()
+        author = request.POST.get('author', '').strip()
+        if title and content and author:
+            article.title = title
+            article.content = content
+            article.author = author
+            article.save()
+            return redirect('articles_detail', article_id=article.id)
+    return render(request, 'articles_edit.html', {'article': article})
+
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'articles_detail.html', {'article': article})
